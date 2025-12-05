250c53138a26feef28b6c01532c333b32570f543
yayop <yayop@example.com>
2025-12-05 20:07:59 +0100

    Revert colormap to winter

--- ZStack_IntensityVsZ.m
+++ ZStack_IntensityVsZ.m
@@ -258,7 +258,7 @@ function [colors, label, relTimes, relSpan, baseCmap] = computeColors(roiData)
 n = numel(roiData);
 times = extractAbsStart(roiData);
 if isempty(times) || all(isnat(times))
-    baseCmap = autumn(256);
+    baseCmap = winter(256);
     colors = baseCmap(round(linspace(1,size(baseCmap,1), n)),:);
     label = 'Video index';
     relTimes = [];
@@ -280,7 +280,7 @@ if mins == maxs
 else
     tnorm = seconds(relTimes - mins) ./ seconds(maxs - mins);
 end
-baseCmap = autumn(256);
+baseCmap = winter(256);
 idx = 1 + round(tnorm*(size(baseCmap,1)-1));
 colors = baseCmap(idx,:);
 end
